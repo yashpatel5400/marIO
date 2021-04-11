028755561e18ecb612b2cb79dc51497420abb726
yashpatel5400 <ypatel5400@gmail.com>
2021-04-11 12:45:01 -0400

    Move the code from over here to over there

--- cartpole_play.py
+++ cartpole_play.py
@@ -32,6 +32,23 @@ import matplotlib.pyplot as plt
 #     torch.backends.cudnn.deterministic = True
 # seed()
 
+### HYPERPARAMS ###
+
+load = True
+run_gpu = True
+run_train = True
+run_test = True
+
+test_epochs = 1_000
+epochs = 30_000
+epsilon = 0.5  # Epsilon-greedy: Chance of choosing random action
+gamma = 0.999  # Discount factor of future rewards
+lr = 0.0001
+
+# Test : 10/100     Prod: 100/10_000
+replay_buffer_capacity = 1_000
+batch_size = 500
+
 
 ### DATA ###
 
@@ -226,6 +243,128 @@ def test_pick_action() -> None:
         print(pick_action(q_s=q_s, epsilon=epsilon))
 
 
+def train(qmodel):
+
+    # Used to predict value of rest of episode (actual Q-value)
+    # - We will update it periodically (target_update_period)
+    # - This reduces variance
+    target_qmodel = None
+    target_update_period = 20  # For more complex environments, this can go a lot higher (few thousand)
+
+    loss_fn = nn.MSELoss()
+    # # Use huber loss instead - https://en.wikipedia.org/wiki/Huber_loss (did not seem to improve model)
+    # loss_fn = nn.functional.smooth_l1_loss
+
+    optimizer = torch.optim.Adam(params=qmodel.parameters(), lr=lr)
+    
+    steps_taken = 0
+    screen = get_screen(env)
+    state = screen - screen
+    for epoch in range(epochs):
+
+        print(epoch)
+        if epoch % target_update_period == 0:
+            target_qmodel = copy.deepcopy(qmodel)
+        if epochs > 1000 and epoch % 200 == 0:
+            print(f"EPOCH {epoch}")
+        # plot_screen(state)  # Debug: ensure that the states look like valid differences (they do)
+        if run_gpu:
+            state = state.cuda()
+        q_s = qmodel.forward(x=state).flatten()
+        a = pick_action(q_s=q_s, epsilon=epsilon)
+
+        # Take a step
+        _state, reward, done, _info = env.step(action=a)
+
+        next_screen = get_screen(env)
+        next_state = next_screen - screen
+        if run_gpu:
+            next_state = next_state.cuda()
+
+        replay_buffer.add(experience=Experience(state=state, action=a, reward=reward, next_state=next_state, done=done))
+        if epoch > batch_size:  # Wait until we have enough experiences for a full batch
+            experiences: List[Experience] = replay_buffer.sample()
+            states, actions, rewards, next_states, dones = map(list, zip(*experiences))
+            states = torch.cat(states)  # Shape: (batch_size, 3, 40, 90)
+            next_states = torch.cat(next_states)  # Shape: (batch_size, 3, 40, 90)
+
+            # Calculate expected Q-values
+            q_s = qmodel.forward(x=states)  # (batch_size, num_actions)
+            q_sa_old = q_s[range(batch_size), actions]
+
+            # Calculate target Q-values
+            q_sp = target_qmodel.forward(x=next_states).max(axis=1).values  # Shape (batch_size,)
+            # Value is zero when state is terminal
+            if run_gpu:
+                q_sp = torch.where(Tensor(dones).to(bool), torch.zeros_like(q_sp).cpu(), q_sp.cpu())
+            else:
+                q_sp = torch.where(Tensor(dones).to(bool), torch.zeros_like(q_sp), q_sp)
+            q_sa_new = Tensor(rewards) + gamma * q_sp
+
+            if run_gpu:
+                loss = loss_fn(q_sa_old.cpu(), q_sa_new.cpu())
+            else:
+                loss = loss_fn(q_sa_old, q_sa_new)
+
+            writer.add_scalar("loss", loss, epoch)
+            qmodel.zero_grad()
+            loss.backward()
+            # # Grad clamp (did not seem to improve model)
+            # for param in qmodel.parameters():
+            #     param.grad.data.clamp_(-1, 1)
+            optimizer.step()
+
+        steps_taken += 1
+        if done:
+            writer.add_scalar("steps_taken", steps_taken, epoch)
+            # print(f"Steps taken: {steps_taken}")
+            steps_taken = 0
+
+        if done:
+            env.reset()
+            screen = get_screen(env)
+            state = screen - screen
+        else:
+            screen = next_screen
+            state = next_state
+
+        # print(f"Epoch: {epoch}\tLoss:{loss}")
+
+    torch.save(qmodel.state_dict(), "dummy")
+
+
+def test():
+    env.reset()
+    screen = get_screen(env)
+    state = screen - screen
+
+    survivals = []
+    cur_run_len = 0
+    for epoch in range(test_epochs):
+        if run_gpu:
+            state = state.cuda()
+        q_s = qmodel.forward(x=state).flatten()
+        a = pick_action(q_s=q_s, epsilon=-1.0) # no longer want random
+
+        # Take a step
+        _, _, done, _ = env.step(action=a)
+
+        next_screen = get_screen(env)
+        next_state = next_screen - screen
+        cur_run_len += 1
+
+        if done:
+            env.reset()
+            screen = get_screen(env)
+            state = screen - screen
+            survivals.append(cur_run_len)
+            cur_run_len = 0
+        else:
+            screen = next_screen
+            state = next_state
+    survivals.append(cur_run_len)
+    print(f"Average: {np.mean(survivals)}")
+
 # test_pick_action()
 
 
@@ -233,16 +372,6 @@ def test_pick_action() -> None:
 # - Good to have inline, so I can cut off training while still having all the variables in my environment
 
 writer = SummaryWriter(comment='_CartPole_target_period_20_lr_0001_batchnorm_huber_loss_target_update_200_grad_clamp')
-
-run_gpu = False
-epochs = 10_000
-epsilon = 0.5  # Epsilon-greedy: Chance of choosing random action
-gamma = 0.999  # Discount factor of future rewards
-lr = 0.0001
-
-# Test : 10/100     Prod: 100/10_000
-replay_buffer_capacity = 1_000
-batch_size = 100
 
 replay_buffer = ReplayBuffer(capacity=replay_buffer_capacity, sample_size=batch_size)
 
@@ -252,93 +381,17 @@ env.reset()
 
 # Set up model
 qmodel = QModel()  # Used to act and generate expected Q values
+
+if load:
+    qmodel.load_state_dict(torch.load("dummy"))
+
 if run_gpu:
     qmodel.cuda()
-# Used to predict value of rest of episode (actual Q-value)
-# - We will update it periodically (target_update_period)
-# - This reduces variance
-target_qmodel = None
-target_update_period = 20  # For more complex environments, this can go a lot higher (few thousand)
 
-loss_fn = nn.MSELoss()
-# # Use huber loss instead - https://en.wikipedia.org/wiki/Huber_loss (did not seem to improve model)
-# loss_fn = nn.functional.smooth_l1_loss
+if run_train:
+    train(qmodel)
 
-optimizer = torch.optim.Adam(params=qmodel.parameters(), lr=lr)
-
-steps_taken = 0
-screen = get_screen(env)
-state = screen - screen
-for epoch in range(epochs):
-
-    print(epoch)
-    if epoch % target_update_period == 0:
-        target_qmodel = copy.deepcopy(qmodel)
-    if epochs > 1000 and epoch % 200 == 0:
-        print(f"EPOCH {epoch}")
-    # plot_screen(state)  # Debug: ensure that the states look like valid differences (they do)
-    if run_gpu:
-        state = state.cuda()
-    q_s = qmodel.forward(x=state).flatten()
-    a = pick_action(q_s=q_s, epsilon=epsilon)
-
-    # Take a step
-    _state, reward, done, _info = env.step(action=a)
-
-    next_screen = get_screen(env)
-    next_state = next_screen - screen
-    if run_gpu:
-        next_state = next_state.cuda()
-
-    replay_buffer.add(experience=Experience(state=state, action=a, reward=reward, next_state=next_state, done=done))
-    if epoch > batch_size:  # Wait until we have enough experiences for a full batch
-        experiences: List[Experience] = replay_buffer.sample()
-        states, actions, rewards, next_states, dones = map(list, zip(*experiences))
-        states = torch.cat(states)  # Shape: (batch_size, 3, 40, 90)
-        next_states = torch.cat(next_states)  # Shape: (batch_size, 3, 40, 90)
-
-        # Calculate expected Q-values
-        q_s = qmodel.forward(x=states)  # (batch_size, num_actions)
-        q_sa_old = q_s[range(batch_size), actions]
-
-        # Calculate target Q-values
-        q_sp = target_qmodel.forward(x=next_states).max(axis=1).values  # Shape (batch_size,)
-        # Value is zero when state is terminal
-        if run_gpu:
-            q_sp = torch.where(Tensor(dones).to(bool), torch.zeros_like(q_sp).cpu(), q_sp.cpu())
-        else:
-            q_sp = torch.where(Tensor(dones).to(bool), torch.zeros_like(q_sp), q_sp)
-        q_sa_new = Tensor(rewards) + gamma * q_sp
-
-        if run_gpu:
-            loss = loss_fn(q_sa_old.cpu(), q_sa_new.cpu())
-        else:
-            loss = loss_fn(q_sa_old, q_sa_new)
-
-        writer.add_scalar("loss", loss, epoch)
-        qmodel.zero_grad()
-        loss.backward()
-        # # Grad clamp (did not seem to improve model)
-        # for param in qmodel.parameters():
-        #     param.grad.data.clamp_(-1, 1)
-        optimizer.step()
-
-    steps_taken += 1
-    if done:
-        writer.add_scalar("steps_taken", steps_taken, epoch)
-        # print(f"Steps taken: {steps_taken}")
-        steps_taken = 0
-
-    if done:
-        env.reset()
-        screen = get_screen(env)
-        state = screen - screen
-    else:
-        screen = next_screen
-        state = next_state
-
-    # print(f"Epoch: {epoch}\tLoss:{loss}")
-
-torch.save(qmodel.state_dict(), "dummy")
+if run_test:
+    test()
 
 env.close()
